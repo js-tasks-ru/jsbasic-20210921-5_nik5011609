019cf43bae2f91b1b166cc9bf14316d6a5ddd26c
Aleksey Belugin <nik5011609@gmail.com>
2021-09-25 13:11:34 +0300

    added file with function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,9 +1,7 @@
 function truncate(str, maxlength) {
   if (str.length > 20) {
-    return str.substr(0, maxlength-1) + "…";
+    return str.substr(0, maxlength - 1) + "…";
   } else {
     return str
   }
 }
-truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
-truncate('Всем привет!', 20);
